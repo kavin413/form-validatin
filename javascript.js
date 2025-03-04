751b4b200b73ed30ac8903364280399a6890076c
kavin413 <srisudhakavin@gmail.com>
2025-03-04 17:00:58 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function validateform() {
     var x = document.forms["myform"]["fname"].value
-    if (x == "") {
-        alert("name must be filled")
+    if (x =< 18) {
+        alert("you must be 18 plus to enter")
         return false
     }
 }
